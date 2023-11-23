4b8705bed7ddf78c2982941db52913c7620db79c
Bastian Löher <b.loeher@gsi.de>
2023-11-23 20:45:22 +0100

    Adjust example to use new classes.

--- examples/point_source_csi.py
+++ examples/point_source_csi.py
@@ -1,10 +1,9 @@
-from gate_macro_gen import Application
+from gate_macro_gen import Application, RootOutput, TreeOutput, Box, Scanner, SinglesDigi, SourceGps, SimulationStatisticActor, DoseActor, EnergySpectrumActor
 
 a = Application()
-a.setMatpath("GateMaterials.db")
-a.setPhysics("emstandard")
 a.setWorld(x = 40, y = 40, z = 80, unit = "cm")
-a.addSourceGps(
+a.setMatpath("GateMaterials.db")
+a.add(SourceGps(
         "gammas",
         particle = "gamma",
         mono = (662., "keV"),
@@ -14,33 +13,47 @@ a.addSourceGps(
                     "radius": (1, "cm"),
                     "halfz": (1, "mm"),
                     "centre": (0, 0, 25, "cm")}
-)
-a.addRootOutput(
+))
+a.add(RootOutput(
         filename = "output",
         flags = ["Hit", "Singles", "Ntuple", "Singles_crystal",
                  "Singles_crystal_readout"]
-)
-a.addTreeOutput(
+))
+a.add(TreeOutput(
         filenames = ["out.root"],
         hits = True,
         collections = ["Singles"]
+))
+crystal = Box(
+        name = "crystal",
+        size = (20, 10, 5, "cm"),
+        position = (0, 0, -20, "cm"),
+        material = "CsITl"
 )
-crystal = {
-        "primitive": "box",
-        "size": (20, 10, 5, "cm"),
-        "position": (0, 0, -20, "cm"),
-        "material": "CsITl",
-        "attach": 1
-}
-a.addDaughter("world", "crystal", "scanner", crystal)
-a.addSinglesDigi("crystal", "adder",
-                 {"positionPolicy": "energyWeightedCentroid"})
-a.addSinglesDigi("crystal", "readout", {"setDepth": 1})
-a.addSinglesDigi("crystal", "energyResolution",
-                 {"fwhm": 0.08, "energyOfReference": (662, "keV")})
-a.addSinglesDigi("crystal", "pileup",
-                 {"setPileupVolume": "crystal", "setPileup": (4, "us")})
+a.add(Scanner("world", crystal, attach=1))
+a.add(SinglesDigi("crystal", "adder",
+                 {"positionPolicy": "energyWeightedCentroid"}))
+a.add(SinglesDigi("crystal", "readout", {"setDepth": 1}))
+a.add(SinglesDigi("crystal", "energyResolution",
+                 {"fwhm": 0.08, "energyOfReference": (662, "keV")}))
+a.add(SinglesDigi("crystal", "pileup",
+                 {"setPileupVolume": "crystal", "setPileup": (4, "us")}))
+a.add(SimulationStatisticActor("stats", "stats.txt"))
+a.add(DoseActor("dose", "dose.mhd", "crystal"))
+a.add(EnergySpectrumActor(
+    "energy", "energy.root", "crystal",
+    spectrum = {"emin": (0, "keV"),
+                "emax": (3, "MeV"),
+                "bins": 2048},
+    letSpectrum = {"emin": (0, "keV/um"),
+                   "emax": (100, "keV/um"),
+                   "bins": 1000},
+    edep = ["Edep", "EdepTime", "EdepTrack", "EdepStep"],
+    eloss = {"emin": (0, "keV"),
+             "emax": (3, "MeV"),
+             "bins": 1024}
+))
 a.setVis({"zoom": 1, "style": "surface", "viewpointThetaPhi": (45, 135),
           "endOfEventAction": "accumulate", "verbose": 2, "axes": True})
-a.start()
+a.print()
 
